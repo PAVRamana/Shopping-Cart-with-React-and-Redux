1b42443f0b40d7d3171a339dc68c130d174bdd4b
PAV RAMANA <pavramana@PAVs-MacBook-Pro.local>
2022-01-30 01:44:22 +0800

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './Resources/css/App.css';
 class App extends Component {
 
   showPage = (path) => {
-    this.props.history.push('/' + path);
+    this.props.history.push('/' + path);console.log("test");
   }
 
   render() {
